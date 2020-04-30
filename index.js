eea422cdfad40956b56fe7361c67ce975cbbb830
tt1702 <0607160026.2008@163.com>
2020-04-30 11:45:45 +0800

    mt jquery

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var count = 0;
 function addCart() {
-    var table = this.parentNode.parentNode.parentNode;
-    var tr = table.rows[0];
+    var table = $(this).parents('table');
+    var tr = table.find('tr:eq(0)');
 
     // var cart = document.getElementById("cart");
     // var cartTr = cart.insertRow(-1);
@@ -10,11 +10,11 @@ function addCart() {
     // cartTr.insertCell(-1).innerHTML = tr.cells[1].innerHTML;
     // cartTr.insertCell(-1).innerHTML = "<input type='button' value='删除' onclick='delCart(this)'>";
     count++;//购物车数量添加
-    document.getElementById("num").innerHTML = count;
+    $("#num").text(count);
     alert("加入购物车成功！");
 }
 
-var btns = document.getElementsByName('buyBtn');
-for(let i=0;i<btns.length;i++){
-    btns[i].onclick = addCart;
-}
+var btns = $('[name=buyBtn]');
+btns.each(function(){
+    $(this).click(addCart);
+})
